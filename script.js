8466c4d5836c688eb1f056cb365b2f7764e43327
Franco Giaverini <giaverini.franco89@gmail.com>
2026-06-07 21:58:45 -0400

    se agregan hobbies

--- script.js
+++ script.js
@@ -1,23 +1,112 @@
 $(document).ready(function () {
-  let veces = 0;
 
-  $("#btn-activar").click(function () {
-    alert("¡Pixel Perfect activada!");
+    let veces = 0;
 
-    $(".carta").addClass("activa");
+    $("#btn-activar").click(function () {
 
-    veces++;
+        alert("¡Pixel Perfect activada!");
 
-    $("#contador").text("Activada " + veces + " veces");
-  });
+        $(".carta").addClass("activa");
 
-  $(".carta").hover(
-    function () {
-      $(".rareza").text("🔥 ¡LEGENDARIA! 🔥");
-    },
+        veces++;
 
-    function () {
-      $(".rareza").text("Legendaria");
-    },
-  );
+        $("#contador").text(
+            "Activada " + veces + " veces"
+        );
+
+    });
+
+    $(".carta").hover(
+
+        function () {
+
+            $(".rareza").text(
+                "🔥 ¡LEGENDARIA! 🔥"
+            );
+
+        },
+
+        function () {
+
+            $(".rareza").text(
+                "Legendaria"
+            );
+
+        }
+
+    );
+
+
+    const hobbies = [
+
+        {
+            nombre: "Gaming",
+            descripcion: "Jugar RPGs y juegos de estrategia en general",
+            horas: 12
+        },
+
+        {
+            nombre: "Deporte",
+            descripcion: "Entrenar y jugar futbol",
+            horas: 14
+        },
+
+        {
+            nombre: "Lectura",
+            descripcion: "Leer comics y manga",
+            horas: 6
+        }
+
+    ];
+
+    let indice =
+        Math.floor(
+            Math.random() * hobbies.length
+        );
+
+    mostrarHobby();
+
+    function mostrarHobby() {
+
+        $("#hobby-nombre").text(
+            hobbies[indice].nombre
+        );
+
+        $("#hobby-descripcion").text(
+            hobbies[indice].descripcion
+        );
+
+        $("#horas-hobby").text("");
+
+    }
+
+    $("#btn-hobby").click(function () {
+
+        $("#hobby").fadeOut(function () {
+
+            indice++;
+
+            if (indice >= hobbies.length) {
+
+                indice = 0;
+
+            }
+
+            mostrarHobby();
+
+            $("#hobby").fadeIn();
+
+        });
+
+    });
+
+    $("#hobby").dblclick(function () {
+
+        $("#horas-hobby").text(
+            "Horas semanales: " +
+            hobbies[indice].horas
+        );
+
+    });
+
 });
